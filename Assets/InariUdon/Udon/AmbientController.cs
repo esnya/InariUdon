d428f84208e5a607287ffc969cbdda1804c15f61
esnya <esnya@nekometer.com>
2020-09-25 08:31:47 +0900

    :sparkles: Add new UDON

---
+++ Assets/InariUdon/Udon/AmbientController.cs
@@ -0,0 +1,27 @@
+﻿namespace EsnyaFactory {
+  using UdonSharp;
+  using UnityEngine;
+  using UnityEngine.Rendering;
+  using VRC.SDKBase;
+  using VRC.Udon;
+
+  public class AmbientController : UdonSharpBehaviour
+  {
+    public AmbientMode ambientMode = AmbientMode.Skybox;
+    public Color ambientEquatorColor = Color.gray;
+    public Color ambientGroundColor = Color.gray;
+    public float ambientIntensity = 1;
+    public Color ambientLight = Color.gray;
+    public Color ambientSkyColor = Color.gray;
+
+    public void Apply()
+    {
+      RenderSettings.ambientMode = ambientMode;
+      RenderSettings.ambientEquatorColor = ambientEquatorColor;
+      RenderSettings.ambientGroundColor = ambientGroundColor;
+      RenderSettings.ambientIntensity = ambientIntensity;
+      RenderSettings.ambientLight = ambientLight;
+      RenderSettings.ambientSkyColor = ambientSkyColor;
+    }
+  }
+}
---
+++ Assets/InariUdon/Udon/TumblerSwitch.cs
@@ -0,0 +1,107 @@
+﻿namespace EsnyaFactory {
+  using UdonSharp;
+  using UnityEngine;
+  using VRC.SDKBase;
+  using VRC.Udon;
+  using VRC.Udon.Common.Interfaces;
+
+  public class TumblerSwitch : UdonSharpBehaviour
+  {
+#if UNITY_EDITOR
+    public new void SendCustomNetworkEvent(NetworkEventTarget target, string eventName)
+    {
+      Debug.Log($"[STUB] SendCustomNetworkEvent, target: {target}");
+      SendCustomEvent(eventName);
+    }
+#endif
+
+    public Animator localAnimator;
+    public string localParameter = "State";
+
+    public AudioSource audioSource;
+
+    public Animator externalAnimator;
+    public string externalParameter = "State";
+
+    public bool state;
+    public bool sync;
+
+    public UdonBehaviour customEventTarget;
+    public string customTurnOnEventName = "TurnOn";
+    public string customTurnOffEventName = "TurnOff";
+
+    private void Start()
+    {
+      if (localAnimator == null)
+      {
+        localAnimator = GetComponent<Animator>();
+      }
+
+      if (audioSource == null)
+      {
+        audioSource = GetComponent<AudioSource>();
+      }
+
+      if (sync) {
+        if (Networking.IsOwner(gameObject)) {
+          SetState(state);
+        }
+      }
+    }
+
+    public override void OnPlayerJoined(VRCPlayerApi player) {
+      if (sync && Networking.IsOwner(gameObject)) {
+        Sync();
+      }
+    }
+
+    override public void Interact()
+    {
+      if (sync) SyncedToggle();
+      else LocalToggle();
+
+      SendCustomNetworkEvent(NetworkEventTarget.All, "PlayAudio");
+    }
+
+    private void LocalToggle()
+    {
+      if (state) TurnOff();
+      else TurnOn();
+    }
+
+    private void SyncedToggle()
+    {
+      if (state) SendCustomNetworkEvent(NetworkEventTarget.All, "TurnOff");
+      else SendCustomNetworkEvent(NetworkEventTarget.All, "TurnOn");
+    }
+
+    public void Sync()
+    {
+      if (state) SendCustomNetworkEvent(NetworkEventTarget.All, "TurnOn");
+      else SendCustomNetworkEvent(NetworkEventTarget.All, "TurnOff");
+    }
+
+    private void SetState(bool newState) {
+      state = newState;
+      if (localAnimator != null) localAnimator.SetBool(localParameter, state);
+      if (externalAnimator != null) externalAnimator.SetBool(externalParameter, state);
+    }
+
+    public void PlayAudio()
+    {
+      if (audioSource != null) audioSource.Play();
+    }
+
+    public void TurnOn()
+    {
+      SetState(true);
+      if (customEventTarget) customEventTarget.SendCustomEvent(customTurnOnEventName);
+    }
+
+    public void TurnOff()
+    {
+      SetState(false);
+      if (customEventTarget) customEventTarget.SendCustomEvent(customTurnOffEventName);
+    }
+  }
+}
