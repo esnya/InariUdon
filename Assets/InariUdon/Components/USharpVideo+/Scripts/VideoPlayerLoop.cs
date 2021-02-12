5dc2b4add83bb92a692c92ae507f61316a5b16d7
esnya <esnya@nekometer.com>
2021-02-12 19:07:57 +0900

    :sparkles: feat(usharp-video+): Apply initial loop state

--- Assets/InariUdon/Components/USharpVideo+/Scripts/VideoPlayerLoop.cs
+++ Assets/InariUdon/Components/USharpVideo+/Scripts/VideoPlayerLoop.cs
@@ -1,23 +1,34 @@
-﻿using UdonSharp;
+using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
+using VRC.SDKBase;
 using VRC.SDK3.Video.Components;
 using VRC.SDK3.Video.Components.AVPro;
 
 namespace EsnyaFactory.InariUdon
 {
-  public class VideoPlayerLoop : UdonSharpBehaviour
-  {
-    public VRCUnityVideoPlayer unityVideoPlayer;
-    public VRCAVProVideoPlayer avProVideoPlayer;
-    public bool loop = false;
-    public Image icon;
+    public class VideoPlayerLoop : UdonSharpBehaviour
+    {
+        public VRCUnityVideoPlayer unityVideoPlayer;
+        public VRCAVProVideoPlayer avProVideoPlayer;
+        public Image icon;
+        public bool loop = false;
 
-    public void Toggle() {
-      loop = !loop;
-      if (unityVideoPlayer != null) unityVideoPlayer.Loop = loop;
-      if (avProVideoPlayer != null) avProVideoPlayer.Loop = loop;
-      if (icon != null) icon.color = loop ? Color.white : Color.gray;
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            if (player.isLocal) UpdateVideoPlayer();
+        }
+
+        public void Toggle() {
+            loop = !loop;
+            UpdateVideoPlayer();
+        }
+
+        void UpdateVideoPlayer()
+        {
+            if (unityVideoPlayer != null) unityVideoPlayer.Loop = loop;
+            if (avProVideoPlayer != null) avProVideoPlayer.Loop = loop;
+            if (icon != null) icon.color = loop ? Color.white : Color.gray;
+        }
     }
-  }
 }
