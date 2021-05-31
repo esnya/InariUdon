86ec363d573d83adb1b9a0ffa3e081def2b498da
esnya <esnya@nekometer.com>
2021-05-31 21:32:33 +0900

    :lipstick: chore: Update UdonToolkit

--- Assets/InariUdon/Misc/Scripts/PickupController.cs
+++ Assets/InariUdon/Misc/Scripts/PickupController.cs
@@ -1,9 +1,8 @@
 using UdonSharp;
 using UdonToolkit;
 using UnityEngine;
-using VRC.SDKBase;
-using VRC.Udon;
 using VRC.Udon.Common.Interfaces;
+using VRC.SDK3.Components;
 
 #if UNITY_EDITOR && !COMPILER_UDONSHARP
 using UdonSharpEditor;
@@ -12,68 +11,70 @@ using UnityEditor;
 
 namespace EsnyaFactory.InariUdon
 {
-    [CustomName("Pickup Controller")]
-    [HelpMessage("Enhancement VRC_Pickup such as relay events or expose Respawn event.")]
-    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    [
+        CustomName("Pickup Controller"),
+        HelpMessage("Enhancement VRC_Pickup such as relay events or expose Respawn event."),
+        UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync),
+        RequireComponent(typeof(VRCPickup)),
+    ]
     public class PickupController : UdonSharpBehaviour
     {
         #region Public Variables
-        [SectionHeader("Respawn")][HelpBox("Set null to use initial world transform")][UTEditor]
+        [SectionHeader("Respawn")][HelpBox("Set null to use initial world transform")]
         public Transform respawnTarget;
         public bool respawnOnDrop = false;
 
-        [Space]
+        [SectionHeader("Collider")]
+        public bool overrideIsTrigger;
 
-        [SectionHeader("OnPickup")][UTEditor]
+        [SectionHeader("OnPickup")]
         public bool fireOnPickup;
 
-        [HideIf("@!fireOnPickup")][UTEditor]
+        [HideIf("@!fireOnPickup")]
         public bool onPickupNetworked;
 
-        [HideIf("@!fireOnPickup")][UTEditor]
+        [HideIf("@!fireOnPickup")]
         public NetworkEventTarget onPickupNetworkTarget;
 
-        [HideIf("@!fireOnPickup")][ListView("OnPickup Target List")][UTEditor]
+        [HideIf("@!fireOnPickup")][ListView("OnPickup Target List")]
         public UdonSharpBehaviour[] onPickupTargets;
         [ListView("OnPickup Target List")]
         public string[] onPickupEvents;
 
-        [Space]
-
-        [SectionHeader("OnDrop")][UTEditor]
+        [SectionHeader("OnDrop")]
         public bool fireOnDrop;
 
-        [HideIf("@!fireOnDrop")][UTEditor]
+        [HideIf("@!fireOnDrop")]
         public bool onDropNetworked;
 
-        [HideIf("@!fireOnDrop")][UTEditor]
+        [HideIf("@!fireOnDrop")]
         public NetworkEventTarget onDropNetworkTarget;
 
-        [HideIf("@!fireOnDrop")][ListView("OnDrop Target List")][UTEditor]
+        [HideIf("@!fireOnDrop")][ListView("OnDrop Target List")]
         public UdonSharpBehaviour[] onDropTargets;
-        [ListView("OnDrop Target List")][Popup("behaviour", "onDropTargets", true)][UTEditor]
+        [ListView("OnDrop Target List")][Popup("behaviour", "onDropTargets", true)]
         public string[] onDropEvents;
-
-
         #endregion
 
         #region Private Variables
         Vector3 initialPosition;
         Quaternion initialRotation;
+        new Collider collider;
         #endregion
-
 
         #region Unity Events
         void Start()
         {
             initialPosition = transform.position;
             initialRotation = transform.rotation;
+            collider = GetComponent<Collider>();
         }
         #endregion
 
         #region VRChat Events
         public override void OnPickup()
         {
+            if (overrideIsTrigger) collider.isTrigger = true;
             if (fireOnPickup) BroadcastCustomEvent(onPickupTargets, onPickupEvents, onPickupNetworked, onPickupNetworkTarget);
         }
 
@@ -81,6 +82,7 @@ namespace EsnyaFactory.InariUdon
         {
             if (respawnOnDrop) Respawn();
             if (fireOnDrop) BroadcastCustomEvent(onDropTargets, onDropEvents, onDropNetworked, onDropNetworkTarget);
+            if (overrideIsTrigger) collider.isTrigger = false;
         }
         #endregion
 
