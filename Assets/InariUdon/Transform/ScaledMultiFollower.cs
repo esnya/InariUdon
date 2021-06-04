b2412afecf716ef6ed43208652754719388b38fe
esnya <esnya@nekometer.com>
2021-06-04 21:23:45 +0900

    :sparkles: feat: Add path searcher into ScaledMultiFollower

--- Assets/InariUdon/Transform/ScaledMultiFollower.cs
+++ Assets/InariUdon/Transform/ScaledMultiFollower.cs
@@ -3,6 +3,7 @@ using UdonToolkit;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using System.Net;
 #if !COMPILER_UDONSHARP && UNITY_EDITOR
 using UdonSharpEditor;
 #endif
@@ -16,11 +17,15 @@ namespace EsnyaFactory.InariUdon
         public Transform sourceParent;
         [HideIf("HideSources")] public Transform[] sources;
         public Transform sourceOrigin;
+        public bool findSourceChild;
+        [HideIf("@!findSourceChild")] public string sourceChildPath;
 
         [SectionHeader("Target")]
         public Transform targetParent;
         [HideIf("HideTargets")] public Transform[] targets;
         public Transform targetOrigin;
+        public bool findTargetChild;
+        [HideIf("@!findTargetChild")] public string targetChildPath;
 
         [SectionHeader("Transforms")]
         public Vector3 positionScale = Vector3.one;
@@ -32,18 +37,22 @@ namespace EsnyaFactory.InariUdon
 
         private int count;
 
-        private Transform[] GetChildren(Transform parent)
+        private Transform[] GetChildren(Transform parent, bool find, string path)
         {
             var count = parent.childCount;
             var children = new Transform[count];
-            for (int i = 0; i < count; i++) children[i] = parent.GetChild(i);
+            for (int i = 0; i < count; i++)
+            {
+                children[i] = parent.GetChild(i);
+                if (find) children[i] = children[i].Find(path);
+            }
             return children;
         }
 
         private void Start()
         {
-            if (sourceParent != null) sources = GetChildren(sourceParent);
-            if (targetParent != null) targets = GetChildren(targetParent);
+            if (sourceParent != null) sources = GetChildren(sourceParent, findSourceChild, sourceChildPath);
+            if (targetParent != null) targets = GetChildren(targetParent, findTargetChild, targetChildPath);
 
             count = Mathf.Min(sources.Length, targets.Length);
         }
@@ -53,9 +62,10 @@ namespace EsnyaFactory.InariUdon
             for (int i = 0; i < count; i++)
             {
                 var source = sources[i];
-                if (ownerOnly && !Networking.IsOwner(source.gameObject)) continue;
+                if (!Utilities.IsValid(source) || ownerOnly && !Networking.IsOwner(source.gameObject)) continue;
 
                 var target = targets[i];
+                if (!Utilities.IsValid(target)) continue;
 
                 var sourcePosition = source.position - (sourceOrigin == null ? Vector3.zero : sourceOrigin.position);
                 var scaledPosition = Vector3.Scale(sourcePosition, positionScale) * scaleMultiplier;
