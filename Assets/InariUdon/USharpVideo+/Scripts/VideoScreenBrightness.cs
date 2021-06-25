b7280eab9735d7024d2fc75fa9384a7b87c22865
esnya <esnya@nekometer.com>
2021-06-25 12:35:17 +0900

    :boom: feat!(usharpvideo-plus): Update USharpVideo to v1

--- Assets/InariUdon/USharpVideo+/Scripts/VideoScreenBrightness.cs
+++ Assets/InariUdon/USharpVideo+/Scripts/VideoScreenBrightness.cs
@@ -4,27 +4,27 @@ using UnityEngine.UI;
 
 namespace EsnyaFactory.InariUdon
 {
-  [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
-  public class VideoScreenBrightness : UdonSharpBehaviour
-  {
-    public Slider slider;
-    public MeshRenderer screen;
-    public int subMesh = 0;
-    public string colorPropertyName = "_EmissionColor";
-    public Image icon;
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class VideoScreenBrightness : UdonSharpBehaviour
+    {
+        public Slider slider;
+        public MeshRenderer screen;
+        public int subMesh = 0;
+        public string colorPropertyName = "_EmissionColor";
+        public Image icon;
 
-    private Material material;
-    private Color max;
+        private Material material;
+        private Color max;
 
-    private void Start() {
-      material = screen.materials[subMesh];
-      max = material.GetColor(colorPropertyName);
+        private void Start() {
+            material = screen.materials[subMesh];
+            max = material.GetColor(colorPropertyName);
+        }
+
+        public void OnSliderValueChanged() {
+            var value = slider.value;
+            material.SetColor(colorPropertyName, max * value);
+            if (icon != null) icon.color = Color.white * value;
+        }
     }
-
-    public void SliderValueChanged() {
-      var value = slider.value;
-      material.SetColor(colorPropertyName, max * value);
-      if (icon != null) icon.color = Color.white * value;
-    }
-  }
 }
