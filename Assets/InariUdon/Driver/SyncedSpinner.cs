09ba937574b263f4f44838336a85ab8f5c0aa61e
esnya <esnya@nekometer.com>
2021-07-10 14:27:26 +0900

    :sparkles: feat: Add SyncedSpinner

---
+++ Assets/InariUdon/Driver/SyncedSpinner.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace InariUdon.Driver
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Continuous)]
+    public class SyncedSpinner : UdonSharpBehaviour
+    {
+        [UdonSynced(UdonSyncMode.Linear)] public float angle = 0.0f;
+        public Vector3 axis = Vector3.up;
+        public float speed = 1.0f;
+        public bool randomizeStartAngle = true;
+
+        private void Start()
+        {
+            axis = axis.normalized;
+            if (Networking.IsOwner(gameObject)) angle = Random.Range(0, 360) % 360;
+        }
+
+        private void Update()
+        {
+            if (Networking.IsOwner(gameObject)) angle += speed * Time.deltaTime % 360.0f;
+            transform.localRotation = Quaternion.AngleAxis(angle, axis);
+        }
+    }
+}
