42ab5be5d26cb88f3417303bf052e572fbce986e
esnya <esnya@nekometer.com>
2021-08-06 19:33:29 +0900

    :bug: fix: Work around of USharpVideo on Unity 2019

--- Assets/InariUdon/USharpVideo+/Scripts/VideoScreenBrightness.cs
+++ Assets/InariUdon/USharpVideo+/Scripts/VideoScreenBrightness.cs
@@ -1,8 +1,9 @@
 ﻿using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
+using UdonToolkit;
 
-namespace InariUdon
+namespace InariUdon.UdonSharpVideoPlus
 {
     [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
     public class VideoScreenBrightness : UdonSharpBehaviour
@@ -11,20 +12,35 @@ namespace InariUdon
         public MeshRenderer screen;
         public int subMesh = 0;
         public string colorPropertyName = "_EmissionColor";
-        public Image icon;
+        public bool uiBrightness;
+        [HideIf("@uiBrightness")] public Image icon;
+        [HideIf("@!uiBrightness")] public Material uiMaterial;
 
         private Material material;
-        private Color max;
+        private Color max, uiMax;
 
         private void Start() {
             material = screen.materials[subMesh];
             max = material.GetColor(colorPropertyName);
+            uiMax = uiMaterial != null ? uiMaterial.GetColor("_Color") : Color.white;
         }
 
         public void OnSliderValueChanged() {
             var value = slider.value;
-            material.SetColor(colorPropertyName, max * value);
-            if (icon != null) icon.color = Color.white * value;
+            var color = Color.white * value;
+            color.a = 1.0f;
+
+            material.SetColor(colorPropertyName, max * color);
+
+            if (uiBrightness)
+            {
+                if (uiMaterial != null)
+                {
+                    color.a = 1.0f;
+                    uiMaterial.SetColor("_Color", uiMax * color);
+                }
+            }
+            else if (icon != null) icon.color = Color.white * color;
         }
     }
 }
