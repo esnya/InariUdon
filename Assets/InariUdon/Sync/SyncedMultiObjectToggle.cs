9d5035c22bf619bac5adfe1eb919f8b244480db6
esnya <esnya@nekometer.com>
2021-11-24 14:50:02 +0900

    :sparkles: feat: SyncedMultiObjectToggle

---
+++ Assets/InariUdon/Sync/SyncedMultiObjectToggle.cs
@@ -0,0 +1,48 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace InariUdon.Sync
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SyncedMultiObjectToggle : UdonSharpBehaviour
+    {
+        public GameObject[] targets = {};
+        public bool initialState = true;
+        [UdonSynced, FieldChangeCallback(nameof(State))] private bool _state;
+        private bool State
+        {
+            set
+            {
+                _state = value;
+                foreach (var target in targets)
+                {
+                    if (target != null) target.SetActive(value);
+                }
+            }
+            get => _state;
+        }
+
+        private void Start()
+        {
+            State = initialState;
+        }
+
+        public override void Interact()
+        {
+            _ToggleActive();
+        }
+
+        public void _SetActive(bool value)
+        {
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            State = value;
+            RequestSerialization();
+        }
+        public void _Activate() => _SetActive(true);
+        public void _Deactivate() => _SetActive(false);
+        public void _ToggleActive() => _SetActive(!State);
+    }
+}
