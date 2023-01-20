a65eae9e32117d442f9ae3a851c801c46418f1e3
esnya <2088693+esnya@users.noreply.github.com>
2023-01-20 22:59:51 +0900

    :sparkles: feat(assets): Load text from assets on build

---
+++ Packages/com.nekometer.esnya.inari-udon/Assets/MultiTextLoader.cs
@@ -0,0 +1,64 @@
+﻿
+using UnityEngine;
+using TMPro;
+
+#if UNITY_EDITOR
+using System.Linq;
+using UnityEditor;
+using VRC.SDKBase.Editor.BuildPipeline;
+#endif
+
+namespace InariUdon
+{
+    public class MultiTextLoader : MonoBehaviour
+    {
+        public string textPath;
+
+#if UNITY_EDITOR
+        private void Load()
+        {
+            var textAssets = AssetDatabase
+                .FindAssets("t:TextAsset", new [] { textPath })
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<TextAsset>)
+                .ToDictionary(t => t.name);
+            foreach (var tmp in GetComponentsInChildren<TMP_Text>(true))
+            {
+                var name = tmp.gameObject.name;
+                if (!textAssets.ContainsKey(name)) continue;
+
+                tmp.text = textAssets[name].text;
+            }
+
+            DestroyImmediate(this);
+        }
+
+        private static void LoadAllInScene()
+        {
+            foreach (var o in FindObjectsOfType(typeof(MultiTextLoader)))
+            {
+                (o as MultiTextLoader)?.Load();
+            }
+        }
+
+        [InitializeOnLoadMethod]
+        public static void Initialize()
+        {
+            EditorApplication.playModeStateChanged += (state) => {
+                if (state == PlayModeStateChange.EnteredPlayMode) LoadAllInScene();
+            };
+        }
+
+        public class BuildCallback : Editor, IVRCSDKBuildRequestedCallback
+        {
+            public int callbackOrder => 0;
+
+            public bool OnBuildRequested(VRCSDKRequestedBuildType requestedBuildType)
+            {
+                LoadAllInScene();
+                return true;
+            }
+        }
+#endif
+    }
+}
