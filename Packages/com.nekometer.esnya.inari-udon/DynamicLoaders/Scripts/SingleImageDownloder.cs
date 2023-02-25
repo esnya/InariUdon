18891c84ea4b56eb3f105d29f6546bbb3ae58ce1
esnya <2088693+esnya@users.noreply.github.com>
2023-02-25 15:54:43 +0900

    :sparkles: feat: Add DynamicLoaders powered by ImageDownloader and StringDownloader

---
+++ Packages/com.nekometer.esnya.inari-udon/DynamicLoaders/Scripts/SingleImageDownloder.cs
@@ -0,0 +1,78 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Image;
+using VRC.SDKBase;
+using VRC.Udon.Common.Interfaces;
+
+namespace InariUdon.DynamicDownloaders
+{
+    /// <summary>
+    /// Download an image from a specified VRCUrl and apply it as a texture to a specified material on a renderer.
+    /// </summary>
+    [RequireComponent(typeof(Renderer))]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class SingleImageDownloder : UdonSharpBehaviour
+    {
+        /// <summary>
+        /// Url of the image to be downloaded.
+        /// </summary>
+        public VRCUrl url;
+
+        /// <summary>
+        /// Index of the material on the renderer that the downloaded texture should be applied to.
+        /// </summary>
+        public int materialIndex;
+
+        /// <summary>
+        /// Minimum delay (in seconds) before the image download begins.
+        /// </summary>
+        public float minDelay = 0;
+
+        /// <summary>
+        /// Maximum delay (in seconds) before the image download begins.
+        /// </summary>
+        public float maxDelay = 30;
+
+        /// <summary>
+        /// Information tells how convert downlded image as texture.
+        /// </summary>
+        public TextureInfo textureInfo;
+
+        /// <summary>
+        /// Temporarily used internally to getting download the texture, and must not be null. It is not used to render the final material, and can be set to any valid material.
+        /// </summary>
+        public Material loaderMaterial;
+
+        private void Start()
+        {
+            SendCustomEventDelayedSeconds(nameof(_Download), Random.Range(minDelay, maxDelay));
+        }
+
+        /// <summary>
+        /// Invoke dowloading.
+        /// </summary>
+        public void _Download()
+        {
+            new VRCImageDownloader().DownloadImage(url, loaderMaterial, (IUdonEventReceiver)this, textureInfo);
+        }
+
+        public override void OnImageLoadSuccess(IVRCImageDownload result)
+        {
+            var properties = new MaterialPropertyBlock();
+            var renderer = GetComponent<MeshRenderer>();
+            var propertyName = result.TextureInfo.MaterialProperty;
+
+            renderer.GetPropertyBlock(properties, materialIndex);
+            properties.SetTexture(propertyName, result.Material.GetTexture(propertyName));
+            renderer.SetPropertyBlock(properties, materialIndex);
+
+            RendererExtensions.UpdateGIMaterials(renderer);
+        }
+
+        public override void OnImageLoadError(IVRCImageDownload result)
+        {
+            Debug.LogError(result, this);
+        }
+    }
+}
---
+++ Packages/com.nekometer.esnya.inari-udon/DynamicLoaders/Scripts/StringDownloader.cs
@@ -0,0 +1,77 @@
+﻿
+using TMPro;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDK3.StringLoading;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+
+namespace InariUdon.DynamicDownloaders
+{
+    /// <summary>
+    /// downloads a string from a specified URL and writes it to a Unity Text component.
+    /// </summary>
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class StringDownloader : UdonSharpBehaviour
+    {
+        /// <summary>
+        /// URL to download the string from.
+        /// </summary>
+        public VRCUrl url;
+
+        /// <summary>
+        /// Minimum delay in seconds before downloading.
+        /// </summary>
+        public float minDelay = 0;
+
+        /// <summary>
+        /// Maximum delay in seconds before downloading.
+        /// </summary>
+        public float maxDelay = 30;
+
+        [Header("Write Target")]
+        /// <summary>
+        /// Component to write the downloaded string to.
+        /// </summary>
+        public Text uiText;
+
+        /// <summary>
+        /// Component to write the downloaded string to.
+        /// </summary>
+        public TextMeshPro textMeshPro;
+
+        /// <summary>
+        /// Component to write the downloaded string to.
+        /// </summary>
+        public TextMeshProUGUI textMeshProUGUI;
+
+        private void Start()
+        {
+            SendCustomEventDelayedSeconds(nameof(_Download), Random.Range(minDelay, maxDelay));
+        }
+
+        /// <summary>
+        /// Invokes downloading.
+        /// </summary>
+        public void _Download()
+        {
+            VRCStringDownloader.LoadUrl(url, (IUdonEventReceiver)this);
+        }
+
+        public override void OnStringLoadSuccess(IVRCStringDownload result)
+        {
+            var text = result.Result;
+
+            if (uiText) uiText.text = text;
+            if (textMeshPro) textMeshPro.text = text;
+            if (textMeshProUGUI) textMeshProUGUI.text = text;
+        }
+
+        public override void OnStringLoadError(IVRCStringDownload result)
+        {
+            Debug.LogError(result, this);
+        }
+    }
+}
